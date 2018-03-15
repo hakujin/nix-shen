38f1bf78a729d1c8cee16de63d22dee2a752e25f
Colin King <richard.colin.king@gmail.com>
2018-03-14 21:27:10 -0500

    Support shen kernel 21.0
    Bumps shen-cl to HEAD which includes the shen 21.0 kernel.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Usage: `nix-env --file default.nix --install shen`
+# Shen for Common Lisp
+
+[Shen](http://www.shenlanguage.org) for Common Lisp by [Mark Tarver](http://marktarver.com/), with contributions by the [Shen Language Open Source Community](https://github.com/Shen-Language).
+
+This is a handy [nix](https://nixos.org/nix/) derivation for the [Steel Bank Common Lisp](http://www.sbcl.org/) port of [shen-cl](https://github.com/Shen-Language/shen-cl).
+
+`nix-env --file default.nix --install shen`
--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ let
   shen-cl = builtins.fromJSON (builtins.readFile ./shen-cl.json);
 in {
   shen = with pkgs; stdenv.mkDerivation {
-    name = "shen-20.1";
+    name = "shen-21.0";
     builder = ./builder.sh;
     src = fetchFromGitHub {
       owner = "Shen-Language";
@@ -11,8 +11,8 @@ in {
       inherit (shen-cl) rev sha256;
     };
     kernel = fetchurl {
-      url = "https://github.com/Shen-Language/shen-sources/releases/download/shen-20.1/ShenOSKernel-20.1.tar.gz";
-      sha256 = "04zjja381ibqm21cj5m5rq8c0gp9w7k33k89dpwcngkdmc4n38y0";
+      url = "https://github.com/Shen-Language/shen-sources/releases/download/shen-21.0/ShenOSKernel-21.0.tar.gz";
+      sha256 = "1m09fjs201y8hzfyalllnmrgw4lhd2cjs7l75860iy0k6pgzwd3h";
     };
     buildInputs = [ sbcl ];
   };
