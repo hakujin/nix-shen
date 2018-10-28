c61eaabeeee0792d97c212640b436c0cf76d4906
Colin King <colin@mercury.co>
2018-10-27 22:50:55 -0500

    Add support for Shen Kernel 21.1
    Adds support for Shen Kernel 21.1 and shen-cl 2.4.

--- builder.sh
+++ builder.sh
@@ -4,7 +4,7 @@ source $stdenv/setup
 mkdir build
 mkdir -p $out/bin
 tar xvfz $kernel
-mv ShenOSKernel-21.0 build/kernel
+mv ShenOSKernel-21.1 build/kernel
 cp -r $src/* build
 
 # building
--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ let
   shen-cl = builtins.fromJSON (builtins.readFile ./shen-cl.json);
 in {
   shen = with pkgs; stdenv.mkDerivation {
-    name = "shen-21.0";
+    name = "shen-21.1";
     builder = ./builder.sh;
     src = fetchFromGitHub {
       owner = "Shen-Language";
@@ -11,8 +11,8 @@ in {
       inherit (shen-cl) rev sha256;
     };
     kernel = fetchurl {
-      url = "https://github.com/Shen-Language/shen-sources/releases/download/shen-21.0/ShenOSKernel-21.0.tar.gz";
-      sha256 = "1m09fjs201y8hzfyalllnmrgw4lhd2cjs7l75860iy0k6pgzwd3h";
+      url = "https://github.com/Shen-Language/shen-sources/releases/download/shen-21.1/ShenOSKernel-21.1.tar.gz";
+      sha256 = "0sfgsmaxag6gxj7ljw98ayxi7w27ml11bw8rgvpvjr4y52hcl7dp";
     };
     buildInputs = [ sbcl ];
   };
