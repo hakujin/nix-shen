e18de7eacc775bcdae71ea1b743a0a3c7c6a41fb
Colin King <colin@mercury.co>
2019-12-19 23:21:35 -0600

    Bump shen-cl to 3.0.3
    Like it says on the tin.

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
 { pkgs ? import <nixpkgs> {} }: {
   shen = with pkgs; stdenv.mkDerivation rec {
-    version = "3.0.2";
+    version = "3.0.3";
     name = "shen-${version}";
     buildInputs = [ sbcl ];
     src = builtins.fetchTarball {
       url = "https://github.com/Shen-Language/shen-cl/releases/download/v${version}/shen-cl-v${version}-sources.tar.gz";
-      sha256 = "0zp1w66mnqcqhbxh6q8j7s5x6fbgyhs3vhddvy9v8d0bqc94yjxc";
+      sha256 = "1yrhgq05q43q8fgazv9b28qvapcdmf95ks6k4c6xwpnkjgzjh530";
     };
     kernel =
       let
